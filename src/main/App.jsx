4b295404f0a6346f1a8611cde1c53d15e1f9bba6
Josoel <cvl.jv1@gmail.com>
2022-04-10 11:48:46 -0300

    usando o router e axios

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,20 +1,20 @@
-import Footer from '../components/template/Footer'
-import Logo from '../components/template/Logo'
-import Main from '../components/template/Main'
-import Nav from '../components/template/Nav'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
-import { HashRouter } from 'react-router-dom'
-import Routes from './Routes'
+import Footer from "../components/template/Footer";
+import Logo from "../components/template/Logo";
+import Main from "../components/template/Main";
+import Nav from "../components/template/Nav";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
 
-
-export default props =>
-    <HashRouter>   
-        <div className='app'>
-            <Logo/>
-            <Nav/>
-            <Routes/>
-            <Footer/>
+export default (props) => (
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
         </div>
-        </HashRouter>
+    </BrowserRouter>
+);
